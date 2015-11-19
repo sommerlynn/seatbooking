ef8fb4d63c8f1fb40f1e3706bd4f7f093d95a0c1
chenpusn <624612156@qq.com>
2015-11-19 13:23:17 +0800

    weixin connect

--- routes/weixin.js
+++ routes/weixin.js
@@ -1,12 +1,9 @@
-/**
- * Created by lenovo on 2015/11/19.
- */
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send("test");
+    res.send(req.query.echostr);
 });
 
 module.exports = router;
