e9944fa4ad9c77bcb9eef866bba7aabd9293490d
chenpusn <624612156@qq.com>
2015-11-19 22:43:57 +0800

    weixin connect

--- routes/weixin.js
+++ routes/weixin.js
@@ -1,8 +1,27 @@
 var express = require('express');
+var sprintf = reqiure("sprintf");
+
 var router = express.Router();
 
-/* GET users listing. */
+var reponsePictureTextMessage = "<xml>"+
+    "<ToUserName><![CDATA[%s]]></ToUserName>"+
+    "<FromUserName><![CDATA[%s]]></FromUserName>"+
+    "<CreateTime>%s</CreateTime>"+
+    "<MsgType><![CDATA[news]]></MsgType>"+
+    "<ArticleCount>1</ArticleCount>"+
+    "<Articles>"+
+    "<item>"+
+    "<Title><![CDATA[%s]]></Title>"+
+    "<Description><![CDATA[%s]]></Description>"+
+    "<PicUrl><![CDATA[%s]]></PicUrl>"+
+    "<Url><![CDATA[%s]]></Url>"+
+    "</item>"+
+    "</Articles>"+
+    "</xml>";
+
+
 router.get('/', function(req, res, next) {
+
     res.send(req.query.echostr);
 });
 
