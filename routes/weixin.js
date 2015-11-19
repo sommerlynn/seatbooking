4764e3252ec15cc3afe2bbcc2377f3f810e1a7d1
chenpusn <624612156@qq.com>
2015-11-19 11:05:20 +0800

    weixin connect
    Signed-off-by: chenpusn <624612156@qq.com>

---
+++ routes/weixin.js
@@ -0,0 +1,12 @@
+/**
+ * Created by lenovo on 2015/11/19.
+ */
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    res.send(req["echostr"]);
+});
+
+module.exports = router;
