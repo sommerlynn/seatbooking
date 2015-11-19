d4125461d0967b5a88de27d1fd12d2f498edc58f
chenpusn <624612156@qq.com>
2015-11-19 12:18:49 +0800

    weixin connect

--- routes/weixin.js
+++ routes/weixin.js
@@ -6,7 +6,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send(req["echostr"]);
+    res.send(req["href"]);
 });
 
 module.exports = router;
