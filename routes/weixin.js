1d55763d54ba91ee3207230b752d3d92b95c4705
chenpusn <624612156@qq.com>
2015-11-20 22:41:55 +0800

    解析客户端消息，并发送反馈
    Signed-off-by: chenpusn <624612156@qq.com>

--- routes/weixin.js
+++ routes/weixin.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var sprintf = reqiure("sprintf");
 
+
 var router = express.Router();
 
-var reponsePictureTextMessage = "<xml>"+
+var receivedTextMessageFromClient = {
+    toUserName: this.toUserName,
+    fromUserName: this.fromUserName,
+    createTime: this.createTime,
+    msgType: this.msgType,
+    content: this.content,
+    msgId: this.msgId
+};
+
+var reponsePictureTextMessageXML = "<xml>"+
     "<ToUserName><![CDATA[%s]]></ToUserName>"+
     "<FromUserName><![CDATA[%s]]></FromUserName>"+
     "<CreateTime>%s</CreateTime>"+
@@ -21,6 +31,56 @@ var reponsePictureTextMessage = "<xml>"+
 
 router.get('/', function(req, res, next) {
     res.send(req.query.echostr);
+});
+
+
+router.post('/', function(req, res, next) {
+    // 将客户端发来的消息（xml格式）转为Json格式 https://github.com/Leonidas-from-XIV/node-xml2js
+    // 客户端收到的消息格式:http://mp.weixin.qq.com/wiki/10/79502792eef98d6e0c6e1739da387346.html
+    //<xml>
+    //<ToUserName><![CDATA[toUser]]></ToUserName>
+    //<FromUserName><![CDATA[fromUser]]></FromUserName>
+    //<CreateTime>1348831860</CreateTime>
+    //<MsgType><![CDATA[text]]></MsgType>
+    //<Content><![CDATA[this is a test]]></Content>
+    //<MsgId>1234567890123456</MsgId>
+    //</xml>
+    var parasString = require("xml2js").parseString;
+    parasString(req.body, function(err, result){
+        var msgObjFromClient = JSON.parse(result);
+
+        // 给客户返回的消息格式 http://mp.weixin.qq.com/wiki/14/89b871b5466b19b3efa4ada8e577d45e.html
+        //<xml>
+        //<ToUserName><![CDATA[toUser]]></ToUserName>
+        //<FromUserName><![CDATA[fromUser]]></FromUserName>
+        //<CreateTime>12345678</CreateTime>
+        //<MsgType><![CDATA[news]]></MsgType>
+        //<ArticleCount>2</ArticleCount>
+        //<Articles>
+        //<item>
+        //<Title><![CDATA[title1]]></Title>
+        //<Description><![CDATA[description1]]></Description>
+        //<PicUrl><![CDATA[picurl]]></PicUrl>
+        //<Url><![CDATA[url]]></Url>
+        //</item>
+        //<item>
+        //<Title><![CDATA[title]]></Title>
+        //<Description><![CDATA[description]]></Description>
+        //<PicUrl><![CDATA[picurl]]></PicUrl>
+        //<Url><![CDATA[url]]></Url>
+        //</item>
+        //</Articles>
+        //</xml>
+        var responseMsg = sprintf(reponsePictureTextMessageXML,
+            msgObjFromClient.fromUserName,
+            msgObjFromClient.toUserName,
+            new Date().getTime(),
+            "小蜓欢迎你",
+            "我可以陪你聊天哦",
+            "http://img5.duitang.com/uploads/item/201503/09/20150309134720_B3zUx.thumb.700_0.jpeg",
+            "http://m.sohu.com");
+        res.send(responseMsg);
+    });
 });
 
 module.exports = router;
