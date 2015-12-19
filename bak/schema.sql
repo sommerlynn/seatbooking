934cb41e9359edf3e97b305ee9449294b9d08fd8
chenpusn <624612156@qq.com>
2015-12-19 10:31:54 +0800

    start again

--- bak/schema.sql
+++ bak/schema.sql
@@ -2,3 +2,12 @@ CREATE VIEW building_area_view AS
 SELECT school_id,building.building_id, building_name, area_id, area_name
 FROM building_area LEFT JOIN building ON building_area.building_id = building.building_id
 ORDER BY building_id ASC, area_id ASC
+
+ALTER VIEW building_area_view AS
+SELECT school_id,building.building_id, building_name, classroom.area_id, area_name,
+COUNT(building.building_id) AS classroom_count, SUM(row_count*column_count) AS seat_count
+FROM classroom LEFT JOIN building_area ON classroom.area_id = building_area.area_id
+LEFT JOIN building ON building_area.building_id = building.building_id
+GROUP BY building.building_id
+
+SELECT WEEKOFYEAR('2015-09-05'),WEEKOFYEAR('2015-09-07'),WEEKOFYEAR(current_date)
