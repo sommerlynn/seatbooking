84ac5e12bc64ba976894af3cca9bf62b850ee506
chenpusn <624612156@qq.com>
2016-01-07 16:20:37 +0800

    微信获取用户信息

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.use(logger('dev'));
 var weixinrobot = require('./lib/weixin-robot');
 require('./lib/weixin-robot/rules')(weixinrobot);
 weixinrobot.watch(app, { token: '1qazxsw2', path: '/weixin' });
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.query());
 
