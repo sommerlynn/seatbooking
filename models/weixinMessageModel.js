ad9ea6bf8263fb1e4e7cbfb20764b376cfd0d659
chenpusn <624612156@qq.com>
2016-01-09 15:59:38 +0800

    通过微信OAuth获取用户信息

--- models/weixinMessageModel.js
+++ models/weixinMessageModel.js
@@ -18,16 +18,38 @@ weixinMessage.logUserLocation = function(openid, lat, lng, callback){
 };
 
 weixinMessage.addUserInfo = function(userInfo, callback){
-    var insertQuery = "insert into user (openid, nickname, sex, city, country, province, headimgurl) "+
-        "values (?,?,?,?,?,?,?)",
-        params = [userInfo.openid, userInfo.nickname, userInfo.sex, userInfo.province,
-            userInfo.city, userInfo.country, userInfo.headimgurl];
-    db.insertQuery(insertQuery, params, function(err, id){
+    var selectQuery = "select user_id from user where openid = ?",
+        selectParams = [userInfo.openid];
+    db.getId(selectQuery, selectParams, function(err, userId){
         if(err){
             callback(err);
         }
-        else{
-            callback(null);
+        else if(userId > 0){
+            var updateQuery = "update user set nickname = ?, sex = ?, city = ?, country = ?, province = ?, headimgurl = ? where openid = ?",
+                updateParams = [userInfo.nickname, userInfo.sex, userInfo.province,
+                    userInfo.city, userInfo.country, userInfo.headimgurl, userInfo.openid];
+
+            db.executeQuery(updateQuery, updateParams, function(err, results){
+                if(err){
+                    callback(err);
+                }
+                else{
+                    callback(null);
+                }
+            });
+        }else{
+            var insertQuery = "insert into user (openid, nickname, sex, city, country, province, headimgurl) "+
+                    "values (?,?,?,?,?,?,?)",
+                insertParams = [userInfo.openid, userInfo.nickname, userInfo.sex, userInfo.province,
+                    userInfo.city, userInfo.country, userInfo.headimgurl];
+            db.insertQuery(insertQuery, insertParams, function(err, id){
+                if(err){
+                    callback(err);
+                }
+                else{
+                    callback(null);
+                }
+            });
         }
     });
 };
