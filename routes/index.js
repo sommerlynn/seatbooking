60d248e6f16143d7542e692caf1bfbcde300271a
chenpusn <624612156@qq.com>
2016-01-10 10:55:58 +0800

    通过微信OAuth实现自动获取用户信息

--- routes/index.js
+++ routes/index.js
@@ -86,13 +86,19 @@ router.post('/order', function(req, res){
     });
 });
 
-router.get('/me/:op', function(req, res){
-    if(req.session.userInfo && req.params.op){
+router.get('/me', function(req, res){
+    if(req.session.userInfo){
         res.render('meView',{title:'我的信息', userInfo:req.session.userInfo});
     }else{
         var client = new OAuth('wxeec4313f49704ee2', '36012f4bbf7488518922ca5ae73aef8e');
         var url = client.getAuthorizeURL('http://www.julyangel.cn/callbackme', '123', 'snsapi_userinfo');
         res.redirect(url);
+    }
+});
+
+router.get('/medebug', function(req, res){
+    if(req.session.userInfo){
+        res.render('meView',{title:'我的信息', userInfo:req.session.userInfo});
     }
 });
 
