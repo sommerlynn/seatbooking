06a0df34c34c2337198ad8bec977f5ba2890675b
chenpusn <624612156@qq.com>
2016-01-10 17:50:45 +0800

    通过微信OAuth实现自动获取用户信息

--- routes/index.js
+++ routes/index.js
@@ -54,20 +54,39 @@ router.get('/seatmap/:cid', function(req, res) {
 * 获取教学楼列表
 * */
 router.get('/building', function(req, res){
-  /*if(req.session.userInfo){
-      models.classroomModel.getAll(1, function(err, classroomList){
-          if(err){
-              res.render('errorView', {message:'服务器故障', error: err});
-          }
-          else{
-              res.render('buildingView', {title:'七玥天使-自习室导航', classroomList: classroomList});
-          }
-      });
-  }else{
-      var client = new OAuth('wxeec4313f49704ee2', '36012f4bbf7488518922ca5ae73aef8e');
-      var url = client.getAuthorizeURL('http://www.julyangel.cn/callbackbuilding', '123', 'snsapi_userinfo');
-      res.redirect(url);
-  }*/
+    /*if(req.session.userInfo){
+     models.classroomModel.getAll(1, function(err, classroomList){
+     if(err){
+     res.render('errorView', {message:'服务器故障', error: err});
+     }
+     else{
+     res.render('buildingView', {title:'七玥天使-自习室导航', classroomList: classroomList});
+     }
+     });
+     }else{
+     var client = new OAuth('wxeec4313f49704ee2', '36012f4bbf7488518922ca5ae73aef8e');
+     var url = client.getAuthorizeURL('http://www.julyangel.cn/callbackbuilding', '123', 'snsapi_userinfo');
+     res.redirect(url);
+     }*/
+    var err = {status:'ok', stack:'ok 111'};
+    res.render('errorView', {title:'预约座位', message:'building', error: err});
+});
+
+router.get('/building2', function(req, res){
+    /*if(req.session.userInfo){
+     models.classroomModel.getAll(1, function(err, classroomList){
+     if(err){
+     res.render('errorView', {message:'服务器故障', error: err});
+     }
+     else{
+     res.render('buildingView', {title:'七玥天使-自习室导航', classroomList: classroomList});
+     }
+     });
+     }else{
+     var client = new OAuth('wxeec4313f49704ee2', '36012f4bbf7488518922ca5ae73aef8e');
+     var url = client.getAuthorizeURL('http://www.julyangel.cn/callbackbuilding', '123', 'snsapi_userinfo');
+     res.redirect(url);
+     }*/
     var err = {status:'ok', stack:'ok 111'};
     res.render('errorView', {title:'预约座位', message:'building', error: err});
 });
