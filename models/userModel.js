e36252d541fafa9cc33412aa6587ff32a399a080
chenpusn <624612156@qq.com>
2016-01-11 20:27:24 +0800

    通过微信OAuth实现自动获取用户信息

--- models/userModel.js
+++ models/userModel.js
@@ -6,7 +6,7 @@ var user = {},
     db = require('./db');
 
 user.order = function(openid, classroomID, row, column, time, callback){
-    var insertQuery = "insert into user_seat_order (user_id, classroom_id, row_no, column_no, start_time) VALUES "+
+    var insertQuery = "insert into user_seat_order (user_id, classroom_id, row_no, column_no, start_time) "+
                       "(select user_id from user where openid = ?, ?, ?, ?, ?)",
         params = [openid, classroomID, row, column, time];
     db.insertQuery(insertQuery, params, function(err, id){
