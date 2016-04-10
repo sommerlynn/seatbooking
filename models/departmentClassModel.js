d4f0baa83268cc341eb608beb02f612aaf5d1a45
chenpusn <qwertyuiop1982>
2016-04-11 04:16:00 +0800

    修改座位预约的界面，改为预约票形式

--- models/departmentClassModel.js
+++ models/departmentClassModel.js
@@ -6,14 +6,14 @@ var departmentClass = {},
     db = require('./db');
 
 departmentClass.getActiveDepartments = function(callback){
-    var selectQuery = "select * from department where is_active = 1",
+    var selectQuery = "select * from department where is_active = 1 order by department_name",
         params = [];
     db.executeQuery(selectQuery, params, callback);
 };
 
 departmentClass.getClass = function (departmentName, callback) {
     var selectQuery = "select * from class where department_id = "+
-                      "(select department_id from department where department_name = ?)",
+                      "(select department_id from department where department_name = ?) order by class_name",
         params = [departmentName];
     db.executeQuery(selectQuery, params, callback);
 };
