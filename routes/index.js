7bd39589e97b289e496c2e20bc27a4983e315467
chenpusn <624612156@qq.com>
2016-04-15 21:14:24 +0800

    增加检测是否由其他人转发的链接。用req.ip来判断。

--- routes/index.js
+++ routes/index.js
@@ -132,14 +132,27 @@ router.get('/me/:openid', function (req, res) {
                                             error: err
                                         });
                                     } else {
-                                        res.render('./meView', {
-                                            ip: req.ip,
-                                            openid: req.params.openid,
-                                            title: '我的信息',
-                                            userInfo: userInfo[0],
-                                            userSeatOrders: userSeatOrders,
-                                            leaveApplications: leaveApplications,
-                                            waitForApprovedLeaveApplications: waitForApprovedLeaveApplications
+                                        var client = new OAuth('wxeec4313f49704ee2', '36012f4bbf7488518922ca5ae73aef8e');
+                                        client.getJsApiTicket(req.params.openid, function(err, result){
+                                            if(err){
+                                                res.render('errorView', {
+                                                    openid: req.params.openid,
+                                                    title: '服务器故障',
+                                                    message: '服务器故障',
+                                                    error: err
+                                                });
+                                            }else{
+                                                res.render('./meView', {
+                                                    ip: req.ip,
+                                                    openid: req.params.openid,
+                                                    jsTicket:result.data.ticket,
+                                                    title: '我的信息',
+                                                    userInfo: userInfo[0],
+                                                    userSeatOrders: userSeatOrders,
+                                                    leaveApplications: leaveApplications,
+                                                    waitForApprovedLeaveApplications: waitForApprovedLeaveApplications
+                                                });
+                                            }
                                         });
                                     }
                                 });
