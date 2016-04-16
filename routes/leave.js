65534086a147160b940d01309d7d949df39c5547
chenpusn <624612156@qq.com>
2016-04-16 22:13:59 +0800

    增加检测是否由其他人转发的链接。用req.ip来判断。

--- routes/leave.js
+++ routes/leave.js
@@ -9,7 +9,8 @@ var express = require('express'),
     xlsx = require('node-xlsx'), // https://github.com/mgcrea/node-xlsx
     models = require('../models'),
     OAuth = require('wechat-oauth'),
-    WeiJSAPI = require('../lib/weixin-jssdk');
+    WeiJSAPI = require('../lib/weixin-jssdk'),
+    urllib = require('urllib');;
 
 var weiJSAPI = new WeiJSAPI('wxeec4313f49704ee2', '36012f4bbf7488518922ca5ae73aef8e');
 
