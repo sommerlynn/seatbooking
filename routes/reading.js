d8ac75fb15119af2e596f2195086820196a91a51
chenpusn <624612156@qq.com>
2016-04-21 21:31:02 +0800

    历史座位

--- routes/reading.js
+++ routes/reading.js
@@ -130,20 +130,19 @@ router.get('/reading/data', function(req, res){
 router.post('/reading/digest', function(req, res){
     models.weixinMessageModel.uploadWeiXinServerResourceToQiniu(req.body.openid, req.body.imageID, 'reading_digest_', function(err, filePath, fileName){
         if(err){
-            //res.send('亲，出错了额，请重试一下' + err.message);
+            res.send('亲，出错了额，请重试一下' + err.message);
         }else{
             sizeOf(filePath, function(err, dimensions){
                 models.readingModel.newDigest(req.body.openid, fileName, dimensions.width, dimensions.height, function(err, result){
                     if(err){
-                        //res.send('亲，出错了额，请重试一下' + err.message);
+                        res.send('亲，出错了额，请重试一下' + err.message);
                     }else{
-                        //res.send('上传成功');
+                        res.send('上传成功');
                     }
                 });
             });
         }
     });
-    res.send('上传成功');
 });
 
 module.exports = router;
