b679494bef46174b2132704d8e0932232f4fde79
chenpusn <624612156@qq.com>
2016-04-23 21:41:50 +0800

    历史座位

--- public/scripts/waterfall.js
+++ public/scripts/waterfall.js
@@ -434,7 +434,7 @@
         },
 
         reload:function(callback){
-            options.state.curPage = 1;
+            this.options.state.curPage = 1;
             this.resume();
             this._init();
         },
