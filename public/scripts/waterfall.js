3d0756bd2efc3016b9ef63d580bd8f78561ba446
chenpusn <624612156@qq.com>
2016-04-23 23:01:31 +0800

    历史座位

--- public/scripts/waterfall.js
+++ public/scripts/waterfall.js
@@ -435,6 +435,7 @@
 
         reload:function(callback){
             this.$element.find('.' + this.options.itemCls).remove();
+            $('#waterfall-message').html('');
             this.options.state.curPage = 1;
             this.resume();
             this._init();
