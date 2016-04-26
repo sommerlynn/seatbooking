c96b59465b8541f85ecb61e7d885513824f67323
chenpusn <qwertyuiop1982>
2016-04-26 10:00:27 +0800

    调整用户信息获取代码

--- routes/seat.js
+++ routes/seat.js
@@ -256,10 +256,10 @@ router.get('/scanclassroom/oauthgetinfo', function (req, res) {
 });
 
 
-router.get('/scanseat/oauth/:schoolID/:cid/:seatcode', function(req, res){
+router.get('/scanseat/oauth/:schoolID/:cid/:seat', function(req, res){
     var url = client.getAuthorizeURL('http://campus.julyangel.cn/scanseat/oauthgetinfo?cid=' +
         req.params.cid + '&schoolID=' + req.params.schoolID+
-        '&seat='+req.params.seatcode, '123', 'snsapi_userinfo');
+        '&seat='+req.params.seat, '123', 'snsapi_userinfo');
     res.redirect(url);
 });
 
