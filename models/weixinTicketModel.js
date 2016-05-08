b4b64214638c2c93be7becb8164940a6f53c6a96
chenpusn <624612156@qq.com>
2016-05-08 15:16:17 +0800

    增加座位计划回收时间相关逻辑

--- models/weixinTicketModel.js
+++ models/weixinTicketModel.js
@@ -4,10 +4,15 @@
  * 2016-05-08：CHEN PU 集中控制oAuth 和 weixinJS 的票据信息
  */
 var oAuth = require('wechat-oauth'),
-    jSAPI = require('../lib/weixin-jssdk');
+    jSAPI = require('../lib/weixin-jssdk'),
+    debug = require('debug'),
+    log = debug('weixin');
 
 var weixinAPIClient = function(appid, appsecret){
+    log('test001');
     if (!(this instanceof weixinAPIClient)) {
+        log('test002');
+
         this._oAuthClient = new oAuth(appid, appsecret);
         this._jSAPIClient = new jSAPI(appid, appsecret);
     }
