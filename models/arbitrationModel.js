a1387a9c5c75594a4a697ad9cab5f5535eaedadf
chenpusn <624612156@qq.com>
2016-05-15 19:19:21 +0800

    申请仲裁 填写页面

--- models/arbitrationModel.js
+++ models/arbitrationModel.js
@@ -6,7 +6,7 @@
 var arbitration = {},
     db = require('./db');
 
-arbitration.new = function(applier_openid, classroom_full_name, seat_code, description){
+arbitration.new = function(applier_openid, classroom_full_name, seat_code, description, callback){
     var insertQuery = "insert into arbitration (applier_openid, classroom_id, seat_code, description) VALUES (?, (select classroom_id from area_classroom_view where full_name = ? ), ?, ?)",
         params = [applier_openid,classroom_full_name,seat_code,description];
     db.executeQuery(insertQuery, params, callback);
