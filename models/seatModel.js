7ad377ce904a3eeb5bf9f90b1118e6d4bc1799df
chenpusn <qwertyuiop1982>
2016-05-16 10:00:37 +0800

    fix bug: 排队轮候逻辑错误

--- models/seatModel.js
+++ models/seatModel.js
@@ -389,7 +389,7 @@ seat.release = function (orderID, openid, callback) {
  * */
 seat.queue = function(openid, classroomID, seatCode, row, column, startTime, endTime, scheduleRecoverTime, callback){
     var insertQuery = "insert into user_seat_order (openid, classroom_id, seat_code, row_no, column_no, start_time, end_time, schedule_recover_time, status, lock_code) values "+
-            "(?, ?, ?, ?, ?, ?, ?, ?, 0, -4)",
+            "(?, ?, ?, ?, ?, ?, ?, ?, 0, 0)",
         insertParams = [openid, classroomID, seatCode, row, column, startTime, endTime, scheduleRecoverTime];
     db.insertQuery(insertQuery, insertParams, function(err, id){
         if(err){
--- models/workerModel.js
+++ models/workerModel.js
@@ -28,12 +28,12 @@ schedule.scheduleJob(rule, function(){
                 //log('系统释放'+orders[index].full_name+' '+orders[index].seat_code+' '+(new Date()).toLocaleString());
                 seatModel.getQueue(orders[index].classroom_id, orders[index].seat_code, function(err, queueOrders){
                     for (var rindex = 0; rindex < queueOrders.length; rindex++){
-                        seatModel.isValidLibraryOrderRequest(orders[index].openid, orders[index].classroom_id, orders[index].seat_code,
-                            orders[index].start_time, orders[index].end_time, function(err, result){
+                        seatModel.isValidLibraryOrderRequest(queueOrders[index].openid, queueOrders[index].classroom_id, queueOrders[index].seat_code,
+                            queueOrders[index].start_time, queueOrders[index].end_time, function(err, result){
                                 if(err){
 
                                 }else{
-                                    seatModel.sign(orders[index].order_id, function(err, result){
+                                    seatModel.sign(queueOrders[index].order_id, function(err, result){
 
                                     });
                                 }
