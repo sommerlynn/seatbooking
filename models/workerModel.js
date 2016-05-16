5e67679f7cd571fbb539d20b14e1fe21a662d6b4
chenpusn <qwertyuiop1982>
2016-05-16 10:07:53 +0800

    fix bug: 排队轮候逻辑错误

--- models/workerModel.js
+++ models/workerModel.js
@@ -24,6 +24,7 @@ rule.hour = hours;
 schedule.scheduleJob(rule, function(){
     seatModel.getOrderNeedToRecycle(function(err, orders){
         for(var index = 0; index < orders.length; index++){
+            log('order_id::'+orders[index].order_id);
             seatModel.sysReleaseAsNotSign(orders[index].order_id, function(err, result){
                 //log('系统释放'+orders[index].full_name+' '+orders[index].seat_code+' '+(new Date()).toLocaleString());
                 seatModel.getQueue(orders[index].classroom_id, orders[index].seat_code, function(err, queueOrders){
