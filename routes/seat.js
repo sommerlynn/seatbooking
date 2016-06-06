40bc7c5efe37ba68394b96bb9cf3f2f2946b32c4
chenpusn <qwertyuiop1982>
2016-06-06 12:37:22 +0800

    空教室查询

--- routes/seat.js
+++ routes/seat.js
@@ -78,7 +78,7 @@ router.get('/emptyClassroom/:area/:sectionStr', function (req, res) {
     var sectionStr = req.params.sectionStr;
 
     models.classroomModel.getEmptyClassroom(area, sectionStr, function(err, classrooms){
-        res.render('./seat/buildingClassroomView',
+        res.render('./seat/EmptyClassroomView',
             {
                 openid: req.params.openid,
                 title: '空教室',
