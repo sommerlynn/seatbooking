7737bf0efaa101a8d9598d879da1615a87078f6e
chenpusn <624612156@qq.com>
2016-06-07 21:28:55 +0800

    计算教室的课程时间 填充classroom_time表

--- models/seatModel.js
+++ models/seatModel.js
@@ -621,12 +621,13 @@ seat.sysReleaseAsNotSign = function(orderID, callback){
  * 2016-05-03: CHEN　PU 新建
  * */
 seat.log = function(orderID, logType, logMsg, callback){
-    var insertQuery = 'insert into seat_log (classroom_id, seat_code, openid, log_type, log_msg) values '+
+    var insertQuery = 'insert into seat_log (classroom_id, seat_code, openid, order_date, log_type, log_msg) values '+
             '((select classroom_id from user_seat_order_view where order_id = ?), '+
             '(select seat_code from user_seat_order_view where order_id = ?), '+
             '(select openid from user_seat_order_view where order_id = ?), '+
+            '(select start_time from user_seat_order_view where order_id = ?), '+
             '?, ?)',
-        insertParams = [orderID, orderID, orderID, logType, logMsg];
+        insertParams = [orderID, orderID, orderID, orderID, logType, logMsg];
     db.insertQuery(insertQuery, insertParams, function(err, id){
         if(err){
             callback(err);
@@ -645,11 +646,12 @@ seat.log = function(orderID, logType, logMsg, callback){
  *
  * */
 seat.logBySpecificUser = function(orderID, openid, logType, logMsg, callback){
-    var insertQuery = 'insert into seat_log (classroom_id, seat_code, openid, log_type, log_msg) values '+
+    var insertQuery = 'insert into seat_log (classroom_id, seat_code, order_date, openid, log_type, log_msg) values '+
             '((select classroom_id from user_seat_order_view where order_id = ?), '+
             '(select seat_code from user_seat_order_view where order_id = ?), '+
+            '(select start_time from user_seat_order_view where order_id = ?), '+
             '?, ?, ?)',
-        insertParams = [orderID, orderID, openid, logType, logMsg];
+        insertParams = [orderID, orderID, orderID, openid, logType, logMsg];
     db.insertQuery(insertQuery, insertParams, function(err, id){
         if(err){
             callback(err);
