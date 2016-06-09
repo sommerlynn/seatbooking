1a47c2d19352ad8a93937489fd732fe333e027d9
chenpusn <624612156@qq.com>
2016-06-09 20:52:31 +0800

    申诉请求

--- app.js
+++ app.js
@@ -72,6 +72,9 @@ app.use('/', reading);
 var data = require('./routes/data');
 app.use('/', data);
 
+var arbitration = require('./routes/arbitration');
+app.use('/', arbitration);
+
 // catch 404 and forward to error handler
 /*app.use(function(req, res, next) {
   var err = new Error('Not Found');
