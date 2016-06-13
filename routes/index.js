151aac3085829a3499c29e0bba1e3ae138b6b904
chenpusn <624612156@qq.com>
2016-06-13 19:18:20 +0800

    座位日志

--- routes/index.js
+++ routes/index.js
@@ -63,6 +63,9 @@ router.get('/oAuthGetInfo', function (req, res) {
             });*/
             weixinAPIClient.jsAPIClient.getUserInfo(openid, function (err, userInfo) {
                 /*if(userInfo.subscribe == 1){*/
+                if(userInfo == null){
+                    res.render('indexView', {openid: openid, title: '七玥校园', message: '请先关注七玥天使微信公众号。'});
+                }else{
                     models.weixinMessageModel.addUserInfo(req.query.schoolID, userInfo, function (err) {
                         if (err) {
                             res.render('errorView', {openid: openid, title: '服务器故障', message: '服务器故障', error: err});
@@ -70,6 +73,8 @@ router.get('/oAuthGetInfo', function (req, res) {
                             res.redirect(req.query.from + '/' + openid);
                         }
                     });
+                }
+
                 /*}else{
                     res.render('indexView', {openid: openid, title: '七玥校园', message: '请先关注七玥天使微信公众号。'});
                 }*/
