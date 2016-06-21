2c235177b20adb7ee82bd6f4cdf2b3ba905f9e2f
chenpusn <qwertyuiop1982>
2016-06-21 10:59:59 +0800

    修改座位图页面 空座时 增加扫码入座的按钮

--- models/userModel.js
+++ models/userModel.js
@@ -46,7 +46,7 @@ user.setAngelCode = function(openid, angelcode, callback){
  *
  * */
 user.getWaitForConfirmList = function(callback){
-    var selectQuery = 'select * from user_info_view where status = 1 order by approve_date',
+    var selectQuery = 'select * from user_info_view where status = 1 order by verify_date',
         selectParams = [];
     db.executeQuery(selectQuery, selectParams, callback);
 };
